a9abd3d1868f14e58704efd493af35db90e2d85d
tarungiridhar <tarungiri1237@gmail.com>
2023-03-26 01:47:59 -0400

    Added connect and disconnect

---
+++ backend.py
@@ -0,0 +1,23 @@
+import os
+
+# Takes in the 
+def connect(code1, code2):
+    file1 = code1+code2+".txt"
+    file2 = code1+code2+".txt"
+    if(not os.path.isfile(file1) and not os.path.isfile(file2)):
+        make = open(file1, "a+")
+        make.close()
+        return("%")
+    elif(not os.path.isfile(file1) and os.path.isfile(file2)):
+        read = open(file2, "r")
+        str = "Successfully connected, missed messages: " + read.read()
+        return(str)
+    else:
+        return("Already connected!")
+
+
+def disconnect(code1, code2):
+    file1 = code1+code2+".txt"
+    os.remove(file1)
+    return("Disconnected!")
+
