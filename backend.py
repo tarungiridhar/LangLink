1ccd09bf57419ce429cab0f14343d3325f930b0a
tarungiridhar <tarungiri1237@gmail.com>
2023-03-26 05:30:57 -0400

    Added Azure Translation to Backend

--- backend.py
+++ backend.py
@@ -1,5 +1,50 @@
 import os
+import requests
+import uuid
+import json
 
+
+def translate(msg, lang1, lang2):
+    # Add your key and endpoint
+    key = "ad13e552a674458f9201e1454bd3267b"
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+
+    # location, also known as region.
+    # required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+    location = "eastus2"
+
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+        'api-version': '3.0',
+        'from': lang1,
+        'to': [lang2]
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        # location required if you're using a multi-service or regional (not global) resource.
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text': msg
+    }]
+
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    #test = json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': '))
+    json_load = (json.loads(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': '))))
+    
+    for x in json_load:
+        return(x['translations'][0]['text'])
+    
+#print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
 # Takes in the user's number as code1 and who they want to connect to as code2
 def connect(code1, code2):
     file1 = code1+code2+".txt"
